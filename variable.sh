d7ef585eaa187ad6c033bbf4148a0a3f68ddf207
Purushotham Maddi <purushothammaddi890@gmail.com>
2026-03-13 19:43:27 +0530

    variable changes

--- variable.sh
+++ variable.sh
@@ -6,4 +6,4 @@ PERSON2=Saikumar
 echo "$PERSON1: Hello $PERSON2, Good Morning"
 echo "$PERSON2: Hi $PERSON1, very Good Morning"
 echo "$PERSON1: How are you $PERSON2?"
-echo "$PERSON2: I'm good, what about you?"
+echo "$PERSON2: I'm good $PERSON1, what about you?"
