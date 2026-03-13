32f71e8829ed3a511d8e4aae3a644989ed17d386
Purushotham Maddi <purushothammaddi890@gmail.com>
2026-03-13 20:00:30 +0530

    args username check

--- variables.sh
+++ variables.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 USERNAME=$1
-PASSWORD=$2
+# echo "please enter your username"
+read USERNAME #the value entered above will be automatically attached to Username variable
+# PASSWORD=$2
 
 echo "username is: $USERNAME, Password is:$PASSWORD"
