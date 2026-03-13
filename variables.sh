aa4bc60cfe4dc57872e6e2d11e12f6019dc1368d
Purushotham Maddi <purushothammaddi890@gmail.com>
2026-03-13 20:10:54 +0530

    args username password validation

--- variables.sh
+++ variables.sh
@@ -5,8 +5,8 @@ echo "please enter your username::"
 read -s USERNAME #the value entered above will be automatically attached to Username variable
 # PASSWORD=$2
 
-echo "username is: $USERNAME" #i am printing just for validation, you should not print username and passwords in scripts.
+# echo "username is: $USERNAME" #i am printing just for validation, you should not print username and passwords in scripts.
 
 echo "Please enter your Password::"
 read -s PASSWORD
-echo "password is: $PASSWORD"
+echo "username is: $USERNAME,password is: $PASSWORD"
