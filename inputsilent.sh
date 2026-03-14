35fb7473517db045fb71b6841ccc52ff0563fcb6
Purushotham Maddi <purushothammaddi890@gmail.com>
2026-03-14 13:08:27 +0530

    password input changes in shell

--- inputsilent.sh
+++ inputsilent.sh
@@ -3,3 +3,6 @@
 #prompt user for password input silent
 read -s -p "Enter your password: " password
 echo   #Move to a new line after password input
+
+#validate or process the password input
+echo "Password entered."
