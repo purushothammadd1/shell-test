e4a5529a250e7c98fd8ccb458913d118bef3d7b9
Purushotham Maddi <purushothammaddi890@gmail.com>
2026-03-14 22:15:50 +0530

    condition stmt added

---
+++ function.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+validate(){
+    if [ $? -ne 0 ]
+    then
+        echo "ERROR:: Installing MySQL is failed"
+        exit 1
+    else
+        echo "Installing MYSQL is Success"
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1 #you can give other than 0
+else
+    echo "Your root user is accessed"
+fi #fi means reverse of if, indicating condition end
+
+yum install mysql -y 
+
+validate
+
+yum install git -y
+
+validate
