0d8e6564729acd3bec5820cf13464d81ecd931b4
Purushotham Maddi <purushothammaddi890@gmail.com>
2026-03-14 12:59:29 +0530

    password input changes in shell

--- inputpassword.sh
+++ inputpassword.sh
@@ -2,3 +2,9 @@
 
 #prompt user for input with a timeout
 read -t 10 -p "Enter your password within 10 seconds: " password
+
+if [ -z "$password" ]; then
+    echo "No Password entered within 10 seconds."
+else
+    echo "Password entered: $password"
+fi 
