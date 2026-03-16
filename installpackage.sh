7226728f6c3be64a3a332ccc1deb92620c0e8f55
Purushotham Maddi <purushothammaddi890@gmail.com>
2026-03-16 22:03:30 +0530

    install pack file  added

--- installpackage.sh
+++ installpackage.sh
@@ -11,7 +11,7 @@ then
     echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1 #you can give other than 0
 else
-    echo "You are root user"
+    echo -e "$G You are root user $N"
 fi #fi means reverse of if, indicating condition end
 
 echo "All arguments passed:: $@"
