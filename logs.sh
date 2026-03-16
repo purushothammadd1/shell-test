88152886d40d0b56f44c58a7039cc70c5541ad21
Purushotham Maddi <purushothammaddi890@gmail.com>
2026-03-16 17:45:29 +0530

    log file changes  added

--- logs.sh
+++ logs.sh
@@ -5,24 +5,28 @@ ID=$(id -u)
 echo "script name: $0"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 LOG_FILE="/tmp/$0-$TIMESTAMP.log"
 
 validate(){
     if [ $1 -ne 0 ]
     then
-        echo "ERROR:: $2 ... failed"
+        echo "ERROR:: $2 ... $R failed $N"
         exit 1
     else
-        echo " $2 ... Success"
+        echo " $2 ... $G Success"
     fi
 }
 
 if [ $ID -ne 0 ]
 then
-    echo "ERROR:: Please run this script with root access"
+    echo "$R ERROR:: Please run this script with root access"
     exit 1 #you can give other than 0
 else
-    echo "Your root user is accessed"
+    echo "Your root $G user is accessed"
 fi #fi means reverse of if, indicating condition end
 
 yum install mysql -y &>> $LOG_FILE 
