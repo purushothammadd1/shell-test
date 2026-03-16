82eedcf1b37faf433ab99ec97cf603183fa9a4c2
Purushotham Maddi <purushothammaddi890@gmail.com>
2026-03-16 17:49:17 +0530

    log file changes  added

--- logs.sh
+++ logs.sh
@@ -23,7 +23,7 @@ validate(){
 
 if [ $ID -ne 0 ]
 then
-    echo "$R ERROR:: Please run this script with root access $N"
+    echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1 #you can give other than 0
 else
     echo "Your root $G user is accessed $N"
