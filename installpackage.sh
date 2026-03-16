85c4213fabd6243be5cf2c7535984c8d8934e194
Purushotham Maddi <purushothammaddi890@gmail.com>
2026-03-16 22:27:29 +0530

    install pack file  added

--- installpackage.sh
+++ installpackage.sh
@@ -18,7 +18,9 @@ VALIDATE(){
         echo -e "$2 ... $R FAILED $N"
     else
         echo -e "$2 ... $G SUCCESS $N"
+    fi
 }
+
 if [ $ID -ne 0 ]
 then
     echo -e "$R ERROR:: Please run this script with root access $N"
