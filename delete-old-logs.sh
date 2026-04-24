a6611b97604afd42a75962f8379d55672c24231e
Purushotham Maddi <purushothammaddi890@gmail.com>
2026-04-24 15:48:59 +0530

    old log sh added

---
+++ delete-old-logs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SOURCE_DIR=/tmp/shellscript-logs
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [! -d $SOURCE_DIR ]
+then
+    echo -e "${R}ERROR:: Source directory $SOURCE_DIR does not exist ${N}"
+fi
+
+FILES_TO_DELETE=$(find . -type f -mtime +14 -name "*.log")
+
+while IFS= read -r file
+do
+    echo "Deleting file: $line"
+done <<< $FILES_TO_DELETE
