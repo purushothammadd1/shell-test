7c8878914ea3707b97800938a9591ab047fa71b4
Purushotham Maddi <purushothammaddi890@gmail.com>
2026-04-24 21:42:14 +0530

    diskusage file changed

--- mail.sh
+++ mail.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-TO_TEAM="$1"
-ALERT_TYPE="$2"
-BODY="$3"
-TO_ADDRESS="$4"
-SUBJECT="$5"
+TO_TEAM=$1
+ALERT_TYPE=$2
+BODY=$3
+ESCAPE_BODY=$(printf '%s\n' "$BODY" | sed -e 's/[]\/$*.^[]/\\&/g');
+TO_ADDRESS=$4
+SUBJECT=$5
 
-ESCAPE_BODY=$(printf '%s\n' "$BODY" | sed ':a;N;$!ba;s/\n/<br>/g' | sed 's/[\/&]/\\&/g')
+FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/BODY/$ESCAPE_BODY/g" template.html)
 
-FINAL_BODY=$(sed -e "s|TO_TEAM|$TO_TEAM|g" \
-                  -e "s|ALERT_TYPE|$ALERT_TYPE|g" \
-                  -e "s|BODY|$ESCAPE_BODY|g" template.html)
-
-echo "$FINAL_BODY" | mail -s "$SUBJECT" -a "Content-Type: text/html" "$TO_ADDRESS"
+echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
