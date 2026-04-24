7742fe10c90b9703d84da9f5ad1db90c21187817
Purushotham Maddi <purushothammaddi890@gmail.com>
2026-04-24 18:32:36 +0530

    disk usage file  added

---
+++ diskusage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
+
+DISK_THRESHOLD=1
+message=""
+
+while IFS= read -r line
+do
+    usage=$(echo $line | awk '{print $6F}' | cut -d'%' -f1)
+    partition=$(echo $line | awk '{print $1F}')
+    if [ $usage -gt $DISK_THRESHOLD ]
+    then
+        message+="High Disk Usage on $partition: $usage%\n"
+    fi
+
+done <<< $DISK_USAGE
